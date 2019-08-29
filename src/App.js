0040c8440bec575b20fb17f7c822d2ebf2779d4f
Harry <harry.mc.winters@gmail.com>
2019-08-28 18:50:25 -0700

    Menu now rendering properly

--- src/App.js
+++ src/App.js
@@ -4,13 +4,42 @@ import Title from './components/title/title';
 import Calculator from './components/calculator/calculator';
 import ParkingMenu from './components/parking-menu/parking-menu';
 
+const parkingInfo = {
+  "Economy Parking": {
+    "Per Hour": 2.00,
+    "Daily Maximum": 9.00,
+    "Per Week": 63.00,
+  },
+  "Short-Term Parking": {
+    "Per Hour": 2.00,
+    "Daily Maximum": 24.00,
+    "Per Week": 168.00,
+  },
+  "Long-Term Surface Parking": {
+    "Per Hour": 2.00,
+    "Daily Maximum": 12.00,
+    "Per Week": 84.00,
+  },
+  "Long-Term Garage Parking": {
+    "Per Hour": 2.00,
+    "Daily Maximum": 17.00,
+    "Per Week": 119.00,
+  },
+  "Valet Parking": {
+    "Per Hour": null,
+     // Valet parking is only charged per day. 
+    "Daily Maximum": 24.00,
+    "Per Week": 169.00,
+  },
+}
+
 function App() {
   return (  
     <div className="App">
       <Title/>
       <div className="Container">
         <Calculator/>
-        <ParkingMenu/>
+        <ParkingMenu parkingInfo={parkingInfo}/>
       </div>
     </div>
   );
--- src/components/parking-menu/parking-menu.js
+++ src/components/parking-menu/parking-menu.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import './parking-menu.css'
 
 function parkingMenu(props){
+    const menu = props.parkingInfo
+    const parkingTypes = Object.keys(menu)
+    
     return (
         <div className="ParkingMenu">
-            I'm a Menu
+            <h3>Parking Options</h3>
+            {parkingTypes.map( (pType) => _makeCatagory(pType, menu))}
         </div>
     )
 }
 
+function _makeCatagory(catagoryName, catagoryObject){
+    let priceCats = Object.keys(catagoryObject[catagoryName])
+    return (
+        <div>
+            <h4>{catagoryName}</h4>
+            {priceCats.map((cat)=>(
+                <div className="PriceList">{cat}: ${catagoryObject[catagoryName][cat]}.00</div>
+            ))}
+        </div>
+    )   
+}
 export default parkingMenu;

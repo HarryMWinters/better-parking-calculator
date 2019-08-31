1dc879c9e3a1834154fff6add73420f6a8adcc9b
HarryMWinters <harrymcwinters@gmail.com>
2019-08-30 19:40:32 -0700

    rel="noopener noreferrer" to links

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -6,14 +6,19 @@ function Title() {
     <Jumbotron>
       <h1>A Better Parking Calculator</h1>
       <p>
-        This project was inspired by an interview challange to "find the bugs"
+        This project was inspired by an interview challenge to "find the bugs"
         in a web app that calculates parking costs.
-        <a href="http://adam.goucher.ca/parkcalc/" target="_blank">
+        <a
+          href="http://adam.goucher.ca/parkcalc/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           {" "}
           Original app
         </a>
         . Background info{" "}
         <a
+          rel="noopener noreferrer"
           href="https://www.infoq.com/news/2010/05/testing_challenge/"
           target="_blank"
         >
@@ -24,11 +29,19 @@ function Title() {
       <p>
         If you're super impressed by bootstrap themes, Fullstack mess-arounds
         and pictures of software engineers in the high Sierra check out my{" "}
-        <a href="https://github.com/HarryMWinters" target="_blank">
+        <a
+          href="https://github.com/HarryMWinters"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           github
         </a>{" "}
         and{" "}
-        <a href="https://www.linkedin.com/in/code-bio/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/code-bio/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           LinkedIn
         </a>
         .
